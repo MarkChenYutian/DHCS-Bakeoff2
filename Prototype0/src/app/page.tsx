1e13b8ddc76237228163c9ca53dec77b1e46d9ef
Yutian Chen <markchenyutian@gmail.com>
2025-03-21 15:22:28 -0400

    Boilerplate

---
+++ Prototype0/src/app/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import Head from 'next/head';
+import React from 'react';
+import '@/lib/env';
+
+
+export default function HomePage() {
+  return (
+    <main>
+      <Head>
+        <meta name="google-site-verification" content="wORtJ7fq4X_rDll9Ym7DJ4lHQvSwbb87d_dflv28rN8" />
+      </Head>
+      <div>Hello</div>
+    </main>
+  );
+}
---
+++ Prototype0/src/constant/config.ts
@@ -0,0 +1,5 @@
+export const siteConfig = {
+  title: 'DHCS Bakeoff 2',
+  description: '',
+  url: 'https://github.com/MarkChenYutian/DHCS-Bakeoff2',
+};
