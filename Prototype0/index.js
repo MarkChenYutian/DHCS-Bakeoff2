dafa387b5b929665244aababb9dcb69a282476a4
tochiai9 <ericaochiai1@gmail.com>
2025-03-23 19:51:43 -0400

    Update index.js

--- Prototype0/index.js
+++ Prototype0/index.js
@@ -145,25 +145,22 @@
      task.start.square.transform({rotation: 0, scale: 1});
      rotateSlider.value = "0";
      scaleSlider.value = "100";
- 
-     // ---- Rotation Slider Handler ----
-     rotateSlider.oninput = function() {
-         task.start.square.transform({
-             rotation: this.value,
-             scale: scaleSlider.value / 100
-         });
-     }
- 
-     // ---- Scale Slider Handler ----
-     scaleSlider.oninput = function() {
-         task.start.square.transform({
-             scale: this.value / 100,
-             rotation: rotateSlider.value
-         });
-     }
 
-     
-     // add some event handlers to the square
+	 // ---- Rotation Slider Handler ----
+rotateSlider.oninput = function() {
+    manipulator.transform({
+        rotation: this.value,
+        scale: scaleSlider.value / 100
+    });
+}
+
+// ---- Scale Slider Handler ----
+scaleSlider.oninput = function() {
+    manipulator.transform({
+        scale: this.value / 100,
+        rotation: rotateSlider.value
+    });
+}
      // https://svgjs.dev/docs/3.0/events/#event-listeners
      let squareClickHandler = function() {
          squareBeingClicked = squareBeingClicked ? false : true;
